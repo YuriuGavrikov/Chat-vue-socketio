6a4c3b11730da3c0271b21dec74aea3e73fdac6d
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2023-10-30 20:54:41 +0300

    add dist

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createPinia } from "pinia";
-import { createApp } from "vue";
-
-import App from "./App.vue";
-
-import "./styles/main.scss";
-
-createApp(App).use(createPinia()).mount("#app");
